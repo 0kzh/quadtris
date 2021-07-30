b5b0005fd1ea0df3725dc28c00390dd8f51309da
Aparajita Ghimire <46252271+aparajitaghimire@users.noreply.github.com>
2021-07-30 17:11:28 -0400

    Update levelFour.cc

--- src/levels/levelFour.cc
+++ src/levels/levelFour.cc
@@ -1,13 +1,15 @@
 #include "levelFour.h"
 
-LevelFour::LevelFour() {
-    levelNum_ = 4;
-}
+LevelFour::LevelFour() { }
 
-char LevelFour::returnBlock() {
-    
-}
+/**
+    int r = rand();  // gives you a number from 0 to RAND_MAX
+    double d = r / RAND_MAX;  // gives you a number from 0 to 1
+    double val = d * 5; // gives you a number from 0 to 5
+*/
+Block LevelThree::makeBlock() {
+    int probs[] = {0,0,1,1,2,2,3,3,4,4,4,4,5,5,5,5,6,6};
+    int r = round(rand() / RAND_MAX * 17);
 
-bool LevelFour::isHeavy() {
-    return true;
+    return Block{static_cast<BlockType>(probs[r]), true};
 }
