71e923e2edf1f9ebb2b11394e932179ed8cef385
Aparajita Ghimire <46252271+aparajitaghimire@users.noreply.github.com>
2021-07-30 17:00:13 -0400

    Level 1 implementation

--- src/levels/levelOne.cpp
+++ src/levels/levelOne.cpp
@@ -2,6 +2,14 @@
 
 LevelOne::LevelOne() {}
 
+/**
+    int r = rand();  // gives you a number from 0 to RAND_MAX
+    double d = r / RAND_MAX;  // gives you a number from 0 to 1
+    double val = d * 5; // gives you a number from 0 to 5
+*/
 Block LevelOne::makeBlock() {
-  return Block{static_cast<BlockType>(rand() % NUM_BLOCK_TYPES - 1), false};
+  int probs[12] = {0,0,1,1,2,2,3,3,4,5,6,6};
+  int r = round(rand() / RAND_MAX * 11);
+
+  return Block{static_cast<BlockType>(probs[r]), false};
 }
