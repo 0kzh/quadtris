950ab5ad239b670037a3ea70ef3799fd3e4de353
Aparajita Ghimire <46252271+aparajitaghimire@users.noreply.github.com>
2021-07-30 17:03:48 -0400

    Level 3 implementation

--- src/levels/levelThree.cc
+++ src/levels/levelThree.cc
@@ -1,13 +1,10 @@
 #include "levelThree.h"
 
-LevelThree::LevelThree() {
-    levelNum_ = 3;
-}
+LevelThree::LevelThree() { }
 
-char LevelThree::returnBlock() {
-    
-}
+Block LevelTwo::makeBlock() {
+  int probs[17] = {0,0,1,1,2,2,3,3,4,4,4,4,5,5,5,5,6,6};
+  int r = round(rand() / RAND_MAX * 17);
 
-bool LevelThree::isHeavy() {
-    return true;
+  return Block{static_cast<BlockType>(probs[r]), true};
 }
