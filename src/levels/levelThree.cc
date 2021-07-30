64797c9e5da56c2fe68ce767d50047b40b704bba
Aparajita Ghimire <46252271+aparajitaghimire@users.noreply.github.com>
2021-07-30 17:08:30 -0400

    Update levelThree.cc

--- src/levels/levelThree.cc
+++ src/levels/levelThree.cc
@@ -2,6 +2,12 @@
 
 LevelThree::LevelThree() { }
 
+
+/**
+    int r = rand();  // gives you a number from 0 to RAND_MAX
+    double d = r / RAND_MAX;  // gives you a number from 0 to 1
+    double val = d * 5; // gives you a number from 0 to 5
+*/
 Block LevelTwo::makeBlock() {
   int probs[17] = {0,0,1,1,2,2,3,3,4,4,4,4,5,5,5,5,6,6};
   int r = round(rand() / RAND_MAX * 17);
