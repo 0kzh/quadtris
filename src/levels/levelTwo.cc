0978a3b211ff6c8194b228dadc3ac65e96bf7851
Aparajita Ghimire <46252271+aparajitaghimire@users.noreply.github.com>
2021-07-30 14:33:33 -0400

    Updated levelTwo.cc

--- src/levels/levelTwo.cc
+++ src/levels/levelTwo.cc
@@ -1,15 +1,7 @@
 #include "levelTwo.h"
 
-LevelTwo::LevelTwo() {
-    levelNum_ = 2;
-}
+LevelTwo::LevelTwo() { }
 
-char LevelTwo::returnBlock() {
-    // random number from [0,6], order is I J L O S Z T
-    int r = round(rand() / RAND_MAX * 6);
-    return blockChars_[r];    
-}
-
-bool LevelTwo::isHeavy() {
-    return false;
+Block LevelOne::makeBlock() {
+  return Block{static_cast<BlockType>(rand() % NUM_BLOCK_TYPES - 1), false};
 }
