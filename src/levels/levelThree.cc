cfd8be92e3fbda0b2bb2fd6b12350d1bbeb1464d
Aparajita Ghimire <46252271+aparajitaghimire@users.noreply.github.com>
2021-07-30 18:10:25 -0400

    Update levelThree.cc

--- src/levels/levelThree.cc
+++ src/levels/levelThree.cc
@@ -12,5 +12,5 @@ Block LevelTwo::makeBlock() {
   int probs[17] = {0,0,1,1,2,2,3,3,4,4,4,4,5,5,5,5,6,6};
   int r = round(rand() / RAND_MAX * 17);
 
-  return Block{static_cast<BlockType>(probs[r]), true};
+  return Block{static_cast<BlockType>(probs[r]), true, 3};
 }
