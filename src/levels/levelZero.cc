eee1e21f668f424654d69d9f55e1749ccdd8691b
Aparajita Ghimire <46252271+aparajitaghimire@users.noreply.github.com>
2021-07-30 22:45:11 -0400

    Level 0 implementation

--- src/levels/levelZero.cc
+++ src/levels/levelZero.cc
@@ -1,10 +1,26 @@
 #include "levelZero.h"
-#include <string>
+#include <iostream>
+#include <fstream>
 
-LevelOne::LevelOne(std::string s) {
-    filename_ = s;
-}
+LevelZero::LevelZero(std::string blkfile) {
+  filename_ = blkfile;
+  cur_ = 0;
+  char blk;
+
+  ifstream blklist (filename_);
+  if (blklist.is_open()) {
+    while (!blklist.eof()) {
+        blklist >> blk;
+        blocks_.push_back(blk);
+    }
+  }
+  blklist.close();
+ }
 
 Block LevelZero::makeBlock() {
-
+  if (cur_ == blocks_.size()) {
+    cur_ = 0;
+  }
+  cur_++;
+  return Block{static_cast<BlockType>blocks_[cur_-1], false, 0};
 }
